b5a6107e329f06db078b472e8fd1bc5a034edea9
yurika sakae <yurika.sun@gmail.com>
2019-09-03 15:09:26 -0700

    get users/user endpoint created

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+
+const server = express();
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The users information couldn't be retrieved" });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(user => {
+      console.log("user", user);
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ error: "the yser with the specified UD does not exist" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The user information couldn't be retrieved" });
+    });
+});
+
+server.listen(5000, () => console.log("server listening on port 5000"));
