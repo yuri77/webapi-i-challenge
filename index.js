126fcd4afce6db4090dab7edd5cfd1b51c064025
yurika sakae <yurika.sun@gmail.com>
2019-09-03 21:45:42 -0700

    post endpoint created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const db = require("./data/db.js");
 
 const server = express();
+server.use(express.json());
 
 server.get("/api/users", (req, res) => {
   db.find()
@@ -15,8 +16,30 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+server.post("/api/users", (req, res) => {
+  console.log(req.body);
+  const user = req.body;
+  db.insert(user)
+    // .then(idObj => db.find())
+    // .then(users => res.status(201).json(users));
+    // .then(Objid =>db.find(Objid.id))
+    .then(({ id }) =>
+     db.findById(id))
+        .then(user => {
+      res.status(201).json(user);
+    })
+        .catch(err=>{console.log(err);
+        res.status(500).json({error:"server error retriving user"})
+    });
+})
+.catch(err=>{
+    console.log(err);
+    res.status(500).json({error:"server error inserting user"
+})
+
 server.get("/api/users/:id", (req, res) => {
-  const id = req.params.id;
+  //const id = req.params.id;
+  const { id } = req.params;
   db.findById(id)
     .then(user => {
       console.log("user", user);
@@ -25,7 +48,7 @@ server.get("/api/users/:id", (req, res) => {
       } else {
         res
           .status(404)
-          .json({ error: "the yser with the specified UD does not exist" });
+          .json({ error: "the user with the specified UD does not exist" });
       }
     })
     .catch(err => {
